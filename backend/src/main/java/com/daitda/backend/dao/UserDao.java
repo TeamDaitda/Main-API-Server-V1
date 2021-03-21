79577b49669ef27f064e49ed276cdf72b0daa64c
Dev-Beom <youngbeom0908@gmail.com>
2021-03-21 16:56:27 +0900

    [Update] 인터페이스/클래스 분리

--- backend/src/main/java/com/daitda/backend/dao/UserDao.java
+++ backend/src/main/java/com/daitda/backend/dao/UserDao.java
@@ -8,50 +8,15 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Repository
-public class UserDao {
-    public static List<User> users;
+public interface UserDao {
 
-    //  실제 데이터는 DB에서 가져오는게 맞으나 아직 DB가 없으니 테스트 용으로 임시 셋팅해놓음
-    static {
-        users = new ArrayList<>();
-        users.add(new User(1, "testName1", "testId1", "1234"));
-        users.add(new User(2, "testName2", "testId2", "1234"));
-        users.add(new User(3, "testName3", "testId3", "1234"));
-        users.add(new User(4, "testName4", "testId4", "1234"));
-        users.add(new User(5, "testName5", "testId5", "1234"));
-    }
+    public List<User> getAllUsers();
 
-    //  Select All user;
-    public List<User> getAllUser(){
-        return users;
-    }
+    public User getUserByUserId(String userId);
 
-    //  Select one user by userId
-    public User getUserByUserId(String userId){
-        return users.stream()
-                .filter(user -> user.getUserId().equals(userId))
-                .findAny()
-                .orElse(new User(-1, "", "", ""));
-    }
+    public User insertUser(User user);
 
-    //  Insert User
-    public User insertUser(User user) {
-        users.add(user);
+    public void updateUser(String userId, User user);
 
-        return user;
-    }
-
-    //  Modify User
-    public void updateUser(String userId, User user) {
-        users.stream()
-                .filter(curUser -> curUser.getUserId().equals(userId))
-                .findAny()
-                .orElse(new User(-1, "", "", ""))
-                .setUserName(user.getUserName());
-    }
-
-    //  Delete User
-    public void deleteUser(String userId) {
-        users.removeIf(user -> user.getUserId().equals(userId));
-    }
+    public void deleteUser(String userId);
 }
