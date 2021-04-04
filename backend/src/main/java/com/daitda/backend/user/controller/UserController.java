5fa96e7ed67364ea59da634702a7b14c691088c4
Dev-Beom <youngbeom0908@gmail.com>
2021-04-04 23:42:06 +0900

    [Update] 상태 코드 업데이트, Swagger Api 문서 추가

--- backend/src/main/java/com/daitda/backend/user/controller/UserController.java
+++ backend/src/main/java/com/daitda/backend/user/controller/UserController.java
@@ -5,69 +5,60 @@ import com.daitda.backend.user.dto.UsersResponseDto;
 import com.daitda.backend.user.dto.UsersSaveRequestDto;
 import com.daitda.backend.user.dto.UsersUpdateRequestDto;
 import com.daitda.backend.user.service.UsersService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMapping("user")
+@RequestMapping("/api/user")
 public class UserController {
 
     private final UsersService usersService;
 
-    /*
-    * 기능명: 사용자 정보 리스트 출력
-    * 설명: 모든 사용자의 정보 리스트를 출력합니다.
-    * */
+    @ApiOperation(value = "사용자 정보 리스트 출력", notes = "모든 사용자의 정보 리스트를 출력합니다.")
     @GetMapping(value = "", produces = "application/json; charset=utf8")
+    @ResponseStatus(HttpStatus.OK)
     public List<UsersListResponseDto> index() {
         return usersService.findAllDesc();
     }
 
-    /*
-    * 기능명: 사용자 등록
-    * 설명: 사용자의 정보를 저장합니다.
-    * */
+    @ApiOperation(value = "사용자 등록", notes = "사용자의 정보를 저장합니다.")
     @PostMapping("")
+    @ResponseStatus(HttpStatus.CREATED)
     public Long save(@RequestBody UsersSaveRequestDto requestDto) {
         return usersService.save(requestDto);
     }
 
-    /*
-    * 기능명: 사용자 기부 카테고리 변경
-    * 설명: 사용자의 기부 카테고리를 변경합니다.
-    * */
-    @PutMapping("/{id}")
+    @ApiOperation(value = "사용자 기부 카테고리 변경", notes = "사용자의 기부 카테고리를 변경합니다.")
+    @PatchMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Long update(@PathVariable Long id, @RequestBody UsersUpdateRequestDto requestDto) {
         return usersService.update(id, requestDto);
     }
 
-    /*
-    * 기능명: 사용자 삭제
-    * 설명: 사용자의 정보를 삭제합니다.
-    * */
+    @ApiOperation(value = "사용자 삭제", notes = "사용자의 정보를 삭제합니다.")
     @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
     public Long delete(@PathVariable Long id) {
         usersService.delete(id);
         return id;
     }
 
-    /*
-    * 기능명: 사용자 아이디로 정보 찾기
-    * 설명: 사용자의 아이디로 사용자의 정보를 찾습니다.
-    * */
+    @ApiOperation(value = "사용자 아이디로 사용자 정보 찾기", notes = "사용자의 아이디로 사용자의 정보를 찾습니다.")
     @GetMapping(value = "/{id}", produces = "application/json; charset=utf8")
+    @ResponseStatus(HttpStatus.OK)
     public UsersResponseDto findById(@PathVariable Long id) {
         return usersService.findById(id);
     }
 
-    /*
-    * 기능명: 사용자 이름으로 정보 찾기
-    * 설명: 사용자의 이름으로 사용자의 정보를 찾습니다.
-    * */
+    @ApiOperation(value = "사용자 이름으로 사용자 정보 찾기", notes = "사용자의 이름으로 사용자의 정보를 찾습니다.")
     @GetMapping(value = "/name/{name}", produces = "application/json; charset=utf8")
+    @ResponseStatus(HttpStatus.OK)
     public List<UsersListResponseDto> findByName(@PathVariable String name) {
         return usersService.findByName(name);
     }
